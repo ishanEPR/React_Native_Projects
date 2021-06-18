26f279986e2b214467afdb3cca5a4bf59058a1ef
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-06-18 12:16:10 +0530

    add google fonts(some errors)

--- UI_Newmorphism/App.js
+++ UI_Newmorphism/App.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React,{useState} from 'react';
+import * as Font from 'expo-font';
 import Home from './screens/home';
+import {AppLoading} from 'expo';
+
+const getFonts= () =>  {
+  return Font.loadAsync({
+  'nunito-regular':require('./assets/fonts/Nunito-Regular.ttf'),
+  'nunito-bold':require('./assets/fonts/Nunito-Bold.ttf')
+});
+} 
 
 export default function App() {
-  return (
+  const [fontLoaded,setFontsLoaded]=useState(false);
+
+  if (fontLoaded) {
+    return (
    <Home/>
   );
+  }else{
+      return(
+        <AppLoading
+        startAsync={getFonts}
+        onFinish={()=>setFontsLoaded(true)}
+        />
+              )
+
+  }
+
+  
 }
 
 
