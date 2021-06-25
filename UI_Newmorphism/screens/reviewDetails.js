3072697811d4a1e1f9af5fe48cf4fd0fefc99aae
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-06-25 16:33:17 +0530

    add rating images

--- UI_Newmorphism/screens/reviewDetails.js
+++ UI_Newmorphism/screens/reviewDetails.js
@@ -1,19 +1,43 @@
 import React from 'react';
-import {StyleSheet,View,Text,Button} from 'react-native';
-import {globalStyles} from '../styles/global';
+import {StyleSheet,View,Text,Image} from 'react-native';
+import {globalStyles,images} from '../styles/global';
 import Card from '../shared/card';
 
 export default function ReviewDetails({navigation})
 {
     
+    const rating=navigation.getParam('rating');
     return(
     <View style={globalStyles.container}>
         <Card>
             <Text style={globalStyles.titleText}>{navigation.getParam('title')}</Text>
             <Text style={globalStyles.titleText}>{navigation.getParam('body')}</Text>
-            <Text style={globalStyles.titleText}>{navigation.getParam('rating')}</Text>
+            {/* <Text style={globalStyles.titleText}>{navigation.getParam('rating')}</Text> */}
+
+            <View style={styles.rating}>
+                <Text >GameZone rating:</Text>
+                <Image style={styles.ratingImage} source={images.ratings[rating]}/>
+
+            </View>
 
         </Card>        
     </View>
     );
+
+
 }
+    const styles=StyleSheet.create({
+        ratingImage:{
+            width:40,
+            height:40
+        },
+        rating:{
+            flexDirection:'row',
+            justifyContent:'center',
+            paddingTop:16,
+            marginTop: 16,
+            borderTopWidth:1,
+            borderTopColor: '#eee'
+        }
+
+    })
--- UI_Newmorphism/styles/global.js
+++ UI_Newmorphism/styles/global.js
@@ -9,3 +9,13 @@ export const globalStyles=StyleSheet.create({
         fontWeight:'bold'
     }
 })
+
+export const images={
+    ratings:{
+        '1': require('../assets/1.png'),
+        '2': require('../assets/icon.png'),
+        '3': require('../assets/3.png'),
+        '4': require('../assets/1.png'),
+        '5': require('../assets/1.png')
+    }
+}; 
