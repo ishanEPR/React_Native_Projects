af25a445b9cc6c04393a10d788aa2480031ade35
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-06-28 20:09:42 +0530

    Whether App Created

--- MusicalPlayer/App.js
+++ MusicalPlayer/App.js
@@ -1,22 +1,27 @@
-//class definition
-import React,{Component} from 'react';
+import React from 'react';
 import {View,Text,StyleSheet,Button} from 'react-native';
-import Sample from './lecture/sample';
 
 
 
 
-export default class App extends Component{
 
-render()
-{
-       return(
-       <View>
-        <Sample/>
+export default function App(){
+
+     return(
+       <View  style={styles.container}>
+          <Text>Ishan Reshmika1</Text>
        </View>
        
   )
-}
 
 
+
 }
+
+const styles=StyleSheet.create({
+   container:{
+     flex:1,
+     alignItems:'center',
+     justifyContent:'center',
+   }
+})
