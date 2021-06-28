f21a8d198c7787e167d5fde0e601959e41ab7785
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-06-28 20:36:18 +0530

    get permission in location

--- MusicalPlayer/App.js
+++ MusicalPlayer/App.js
@@ -1,18 +1,43 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import {View,Text,StyleSheet,Button} from 'react-native';
-
+import * as Location from 'expo-location'
+ 
 
 
 
 
 export default function App(){
 
+  useEffect(() =>{
+    load()
+
+  },[])
+  const [errorMessage,setErrorMessage]=useState(null)
+
+  async function load(){
+    try {
+      let {status}=await Location.requestPermissionAsync()
+
+      if(status != 'granted'){
+        setErrorMessage('Access to location is needed to run the app')
+        return 
+      }
+      
+      
+    } catch (error) {
+      
+    }
+  }
+  
+
+
+
      return(
        <View  style={styles.container}>
           <Text>Ishan Reshmika1</Text>
        </View>
        
-  )
+  );
 
 
 
