4519d3b4fcbde10fa86886022dad680a2f1c3365
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-07-09 01:40:10 +0530

    style the list item

--- NewMusicalPlayer/App.js
+++ NewMusicalPlayer/App.js
@@ -3,17 +3,25 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import AppNavigator from './app/navigation/AppNavigator';
 import AudioProvider from './app/context/AudioProvider';
+import AudioListItem from './app/components/AudioListItem';
 
 export default function App() {
-  return (
-   <AudioProvider>
-       <NavigationContainer>
-        <AppNavigator/>
+  // return (
+  //  <AudioProvider>
+  //      <NavigationContainer>
+  //       <AppNavigator/>
 
     
-    </NavigationContainer>
-   </AudioProvider>
+  //   </NavigationContainer>
+  //  </AudioProvider>
 
+  // );
+
+  return (
+    <View style={{marginTop:50}}>
+        <AudioListItem/>
+
+    </View>
   );
 }
 
