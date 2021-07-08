367bac7a2abeb710b8005a1b35b231ea54c3a4ef
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-07-08 12:35:22 +0530

    add template files

---
+++ NewMusicalPlayer/app/screens/AudioList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { View, Text,StyleSheet } from 'react-native';
+
+export default function AudioList() {
+    return (
+        <View style={style.container}>
+            <Text>Audio List</Text>
+        </View>
+    )
+}
+const styles=StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    }
+})
