35ef2df206c7301e7808715a204b4d662a0454e7
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-07-08 16:56:04 +0530

    mediaLibrary async display

--- NewMusicalPlayer/app/screens/AudioList.js
+++ NewMusicalPlayer/app/screens/AudioList.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import { View, Text,StyleSheet } from 'react-native';
+import * as MediaLibrary from 'expo-media-library';
 
 export default function AudioList() {
+
+useEffect(() => {
+    MediaLibrary.getPermissionsAsync().then(data => {
+      console.log(JSON.stringify(data));
+    });
+  }, []);
     return (
         <View style={styles.container}>
             <Text>Audio List</Text>
