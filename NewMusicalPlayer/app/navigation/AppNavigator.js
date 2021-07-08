494644847da7fdfda6abfb27e83f1a94a5631446
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-07-08 13:15:44 +0530

    style bottom tab

--- NewMusicalPlayer/app/navigation/AppNavigator.js
+++ NewMusicalPlayer/app/navigation/AppNavigator.js
@@ -3,14 +3,34 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import AudioList from '../screens/AudioList';
 import Player from '../screens/Player';
 import PlayList from '../screens/PlayList';
+import { Ionicons,FontAwesome5,MaterialIcons } from '@expo/vector-icons';
 
 const Tab=createBottomTabNavigator();
 const AppNavigator = () => {
     return (
        <Tab.Navigator>
-            <Tab.Screen name='AudioList' component={AudioList}/>
-            <Tab.Screen name='Player'  component={Player}/>
-            <Tab.Screen name='PlayList' component={PlayList}/>
+            <Tab.Screen name='AudioList' component={AudioList}
+            options={{
+                tabBarIcon: ({color,size})=>(
+                     <Ionicons name="headset" size={size} color={color} />
+                ),
+
+            }}
+            />
+            <Tab.Screen name='Player'  component={Player}
+            options={{
+                tabBarIcon: ({color,size})=>(
+                    <FontAwesome5 name="compact-disc" size={size} color={color} />
+                )
+            }}
+            />
+            <Tab.Screen name='PlayList' component={PlayList}
+            options={{
+                tabBarIcon: ({color,size})=>(
+                    <MaterialIcons name="library-music" size={size} color={color} />
+                )
+            }}
+            />
        </Tab.Navigator>
     )
 }
